5d5dd439a18e8ed7b6af7aba09fe989e4251b233
jasl <jasl9187@hotmail.com>
2018-01-25 03:27:05 +0800

    Using MRI 2.5.0 in dev, integrate rubocop

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 apt-get update
 apt-get install -y build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev wget libffi-dev gcc g++ make bison libtool autoconf git
 cd `mktemp -d`
-curl -L "https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.3.tar.gz" | tar -xzf-
-cd ruby-2.3.3
+curl -L "https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.0.tar.gz" | tar -xzf-
+cd ruby-2.5.0
 ./configure --prefix=/usr/local
 make
 make install
