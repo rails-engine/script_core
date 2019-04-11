1cf7f5010fb1d82bde787a9dbf987dc6041b2deb
jasl <jasl9187@hotmail.com>
2019-04-12 03:27:57 +0800

    use ruby 2.6.2

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev wget libffi-dev gcc g++ make bison libtool autoconf git
 cd `mktemp -d`
 curl -L "https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.3.tar.gz" | tar -xzf-
-cd ruby-2.5.3
+cd ruby-2.6.2
 ./configure --prefix=/usr/local
 make
 make install
