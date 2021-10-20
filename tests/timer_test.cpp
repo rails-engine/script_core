2874a60810627878e352f17d4cecc8461b6e3d64
jasl <jasl9187@hotmail.com>
2021-10-21 07:15:48 +0800

    Port Shopify/ess#51 to support ARM and M1 chips

--- tests/timer_test.cpp
+++ tests/timer_test.cpp
@@ -4,11 +4,6 @@
 #include "timer.hpp"
 #include "gtest/gtest.h"
 
-TEST(timer_test, measure_cpu_time_scale_on_init) {
-  timer t(nullptr);
-  EXPECT_GT(t.cpu_time_scale_, std::uint64_t{0});
-}
-
 TEST(timer_test, writes_output_once) {
   int invokes = 0;
   auto w = [&invokes](const std::string, const std::int64_t) {
@@ -39,5 +34,18 @@ TEST(timer_test, writer_gets_proper_timing) {
   {
     auto scope = t.measure("foo");
     usleep(time);
+  }
+}
+
+TEST(timer_test, get_elapsed_time_us_returns_proper_time) {
+  std::int64_t time = 42;
+  auto w = [time](const std::string, const std::int64_t) { };
+  timer t(w);
+
+  {
+    auto scope = t.measure("foo");
+    EXPECT_LT(scope.get_elapsed_time_us(), time);
+    usleep(time);
+    EXPECT_GE(scope.get_elapsed_time_us(), time);
   }
 }
