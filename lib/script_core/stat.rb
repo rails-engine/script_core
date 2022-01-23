4d9c6c418fb794d2043d66bc20e0f674bfc110e3
jasl <jasl9187@hotmail.com>
2022-01-23 23:17:41 +0800

    Port ESS#50 Add script execution time to stat

--- lib/script_core/stat.rb
+++ lib/script_core/stat.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
 module ScriptCore
-  Stat = Struct.new(:instructions, :memory, :bytes_in, :time, :total_instructions) do
+  Stat = Struct.new(
+    :instructions,
+    :memory,
+    :bytes_in,
+    :time,
+    :execution_time_us,
+    :total_instructions
+  ) do
     def initialize(options)
-      super(options[:instructions], options[:memory], options[:bytes_in], options[:time], options[:total_instructions])
+      super(
+        options[:instructions],
+        options[:memory],
+        options[:bytes_in],
+        options[:time],
+        options[:execution_time_us],
+        options[:total_instructions]
+      )
     end
   end
 
@@ -12,6 +26,7 @@ module ScriptCore
     memory: 0,
     bytes_in: 0,
     time: 0,
+    execution_time_us: 0,
     total_instructions: 0
   )
 end
--- spec/script_core/stat_spec.rb
+++ spec/script_core/stat_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe(ScriptCore::Stat) do
   let(:null_stat) { ScriptCore::Stat::Null }
 
   it "supports all stats" do
-    options = { instructions: 1, memory: 2, bytes_in: 3, time: 4, total_instructions: 5 }
+    options = { instructions: 1, memory: 2, bytes_in: 3, time: 4, execution_time_us: 5, total_instructions: 6}
     stat = ScriptCore::Stat.new(options)
     expect(stat).to have_attributes(options)
   end
 
   it "nullStats are all zero" do
-    default_values = { instructions: 0, memory: 0, bytes_in: 0, time: 0, total_instructions: 0 }
+    default_values = { instructions: 0, memory: 0, bytes_in: 0, time: 0, execution_time_us: 0, total_instructions: 0 }
     expect(null_stat).to have_attributes(default_values)
   end
 end
